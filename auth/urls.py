1ad8324776d04c94c969138425bad1e25297a991
Edi Kartono <bisnis@edikartono.com>
2023-07-02 15:45:21 +0700

    vercel

--- auth/urls.py
+++ auth/urls.py
@@ -14,10 +14,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework.schemas import get_schema_view
+from django.conf.urls.static import static
 from django.views.generic import TemplateView
+
+from rest_framework.schemas import get_schema_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -32,3 +35,6 @@ urlpatterns = [
         version="1.0.0"
     ), name='api_schema'),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
